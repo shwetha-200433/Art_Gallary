8f5812112e51d7381acb4f02ac2d843751deacf5
shwetha-200433 <shwethashwre@gmail.com>
2025-05-07 00:32:48 +0530

    Add index.html

---
+++ boot.js
@@ -0,0 +1,16 @@
+// let changemode=document.querySelector(".mode");
+// let mode="light";
+// changemode.addEventListener("click",()=>{
+//     if(mode=="light"){
+//         mode="dark";
+//         document.querySelector("body").style.backgroundColor="black",color="white";
+//         document.querySelector("body").style.Color="white";
+
+//     }
+//     else{
+//         mode="light";
+//         document.querySelector("body").style.backgroundColor="white";
+//         document.querySelector("body").style.Color="black";
+//     }
+//     console.log(mode);
+// });
